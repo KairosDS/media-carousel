80bb95fcbc4aebfb41f169b443d5761fcf45d546
Marta <marta.aguilar@kairosds.com>
2021-02-09 10:20:42 +0100

    add buttons styles

--- src/media-carousel-style.js
+++ src/media-carousel-style.js
@@ -6,20 +6,24 @@ export const wcNameStyles = css`
     width: 90vw;
   }
 
+  .media-carousel__content {
+    display: flex;
+  }
+
   .media-carousel__container {
-        width: calc(1200px);
-        align-items: center;
-        display: flex;
-        justify-content: space-between;
-        overflow-x: hidden;
-        overflow-y: hidden;
-      }
+    width: calc(1200px);
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    overflow-x: hidden;
+    overflow-y: hidden;
+  }
       
-      .media-carousel__wrapper {
-        height: 100%;
-        position: relative;
-        transition: left ease 0.8s;
-      }
+  .media-carousel__wrapper {
+    height: 100%;
+    position: relative;
+    transition: left ease 0.8s;
+  }
 
   .media-carousel__list {
     align-items: center;
@@ -27,12 +31,27 @@ export const wcNameStyles = css`
     justify-content: space-between;
     overflow-x: hidden;
     overflow-y: hidden;
+    list-style: none;
   }
 
   li{
     width: 200px;
     height: 100px;
+    border: 3px solid #b8f2e6;
   }
+
+  .media-carousel__button {
+    background-color: transparent;
+    border: none;
+  }
+
+  .media-carousel__arrow {
+    width: 20px;
+  }
+
+
+
+
 
   @media all and (min-width: 1024px) {
    
